ef6e2bd7919d937e9b650eb6b79154957fb84c3b
aleksei <dallasopen@hotmail.com>
2018-03-21 04:13:11 +0200

    Added prefix api/ to all urls, added obtaining and refreshing token

--- api/urls.py
+++ api/urls.py
@@ -1,29 +1,32 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
+from rest_framework_jwt import views as jwt_views
+from djoser import views as djoser_views
 from .views import *
 
 urlpatterns = {
-	url(r'^auth/obtain_token/', obtain_jwt_token),
-	url(r'^auth/refresh_token/', refresh_jwt_token),
-	url(r'^users/$', ProfilesView.as_view(), name="create"),
-	url(r'^users/(?P<pk>[0-9]+)/$',
+	# url(r'^user/logout/all/$', views.SpaUserLogoutAllView.as_view(), name='user-logout-all'),
+	url(r'^api/login/$', jwt_views.ObtainJSONWebToken.as_view(), name='user-login'),
+	url(r'^api/login/refresh/$', jwt_views.RefreshJSONWebToken.as_view(), name='user-login-refresh'),
+	url(r'^api/users/$', ProfilesView.as_view(), name="create"),
+	url(r'^api/users/(?P<pk>[0-9]+)/$',
 		DetailsProfileView.as_view(), name="details"),
-	url(r'^events/$', EventsView.as_view(), name="create"),
-	url(r'^events/(?P<pk>[0-9]+)/$',
+	url(r'^api/events/$', EventsView.as_view(), name="create"),
+	url(r'^api/events/(?P<pk>[0-9]+)/$',
 		DetailsEventView.as_view(), name="details"),
-	url(r'^predictions/$', PredictionsView.as_view(), name="create"),
-	# url(r'^predictions/(?P<pk>[0-9]+)/$',
-	# 	DetailsPredictionView.as_view(), name="details"),
-	url(r'^options/$', OptionsView.as_view(), name="create"),
-	url(r'^options/(?P<pk>[0-9]+)/$',
+	url(r'^api/predictions/$', PredictionsView.as_view(), name="create"),
+	url(r'^api/accuratepredictions/$', AccuratePredictionsView.as_view(), name="create"),
+	url(r'^api/parleyevents/$', ParleyEventsView.as_view(), name="create"),
+	url(r'^api/options/$', OptionsView.as_view(), name="create"),
+	url(r'^api/options/(?P<pk>[0-9]+)/$',
 		DetailsOptionView.as_view(), name="details"),
-	url(r'^bets/$', BetsView.as_view(), name="create"),
-	url(r'^bets/(?P<pk>[0-9]+)/$',
+	url(r'^api/bets/$', BetsView.as_view(), name="create"),
+	url(r'^api/bets/(?P<pk>[0-9]+)/$',
 		DetailsBetView.as_view(), name="details"),
-	url(r'^accuratebets/$', AccurateBetsView.as_view(), name="create"),
-	url(r'^parleys/$', ParleysView.as_view(), name="create"),
-	url(r'^parleys/(?P<pk>[0-9]+)/$',
+	url(r'^api/accuratebets/$', AccurateBetsView.as_view(), name="create"),
+	url(r'^api/parleys/$', ParleysView.as_view(), name="create"),
+	url(r'^api/parleys/(?P<pk>[0-9]+)/$',
 		DetailsParleyView.as_view(), name="details"),
 }
 	
