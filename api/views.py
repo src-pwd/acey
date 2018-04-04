70bab50c84fe10504a9c15fd9c9cfafc337f45d6
aleksei <dallasopen@hotmail.com>
2018-04-04 11:28:42 +0300

    Trying email

--- api/views.py
+++ api/views.py
@@ -8,8 +8,23 @@ from rest_framework.permissions import *
 from .models import Profile
 from .serializers import *
 from rest_framework.metadata import SimpleMetadata
+from django.template.loader import render_to_string
+
 # from .permissions import IsOwner
 
+# def activate(request, uidb64, token):
+#     try:
+#         uid = force_text(urlsafe_base64_decode(uidb64))
+#         user = User.objects.get(pk=uid)
+#     except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+#         user = None
+
+#     if user is not None and account_activation_token.check_token(user, token):
+#         user.is_active = True
+#         user.save()
+#         return HttpResponse('Accepted', status=202)
+#     else:
+#         return HttpResponse('Unauthorized', status=401)
 
 class ProfilesView(generics.ListCreateAPIView):
     queryset = Profile.objects.all()
