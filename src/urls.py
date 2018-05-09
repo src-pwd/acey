12c7105afd09ae9e4e0fbde802602dfc7cb804c7
aleksei <dallasopen@hotmail.com>
2018-05-09 23:28:14 +0300

    a

--- src/urls.py
+++ src/urls.py
@@ -21,7 +21,7 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    url(r'^.*$', TemplateView.as_view(teplate_name="/root/index.html")),
+    url(r'^$', 'django.contrib.staticfiles.views.serve', kwargs={'path': 'index.html', 'document_root': 'root'}),
     url(r'^admin/', admin.site.urls),
     url(r'^', include ('api.urls', namespace='api', app_name='api')),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.activate, name='activate'),
