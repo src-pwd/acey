191c713364eb5421b68c1f94d4c7c403b1f7f9f1
aleksei <dallasopen@hotmail.com>
2018-05-13 00:06:06 +0300

    Hotfix

--- api/serializers.py
+++ api/serializers.py
@@ -69,7 +69,7 @@ class ProfileSerializer(serializers.ModelSerializer):
 			except IntegrityError:
 				raise serializers.ValidationError("User with username '{}' already exists.".format(user.username))
 			profile = Profile.objects.create(user=user, profile_picture = "{0}.svg".format(user.username), sum = 1000, **validated_data)
-		mail_subject = 'Activate your blog account.'
+		mail_subject = 'Activate your ACEY account.'
 		message = render_to_string('acc_active_email.html', {
 				'user': user,
 				'domain': DOMAIN,
